725f6ab76375c75bc369b5fc1f2f105910b612d2
singhharshit24 <singhharshit2409@gmail.com>
2023-03-11 18:35:46 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './Components/Navbar';
+import SignIn from './Pages/login';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Jobs from './Pages/jobs';
+import VideoCall from './Pages/VideoCall';
+import Homepage from './Pages/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* <Route exact path='/' element={<Homepage />} /> */}
+          <Route exact path='/signin' element={<SignIn />} />
+          <Route exact path='/jobs' element={<Jobs />} />
+          <Route exact path='/video-call' element={<VideoCall />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,63 @@
+// import React from "react";
+// import { makeStyles } from '@mui/material/Styles';
+// import Grid from '@mui/material/Grid';
+// import Typography from '@mui/material/Typography';
+// import Button from '@mui/material/Button';
+
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//         flexGrow: 1,
+//         margin: theme.spacing(4),
+//     },
+//     title: {
+//         fontWeight: 700,
+//         marginBottom: theme.spacing(2),
+//     },
+//     subtitle: {
+//         marginBottom: theme.spacing(2),
+//     },
+//     button: {
+//         marginRight: theme.spacing(2),
+//     },
+// }));
+
+// function Homepage() {
+//     const classes = useStyles();
+
+//     return (
+//         <div className={classes.root}>
+//             <Grid container spacing={3}>
+//                 <Grid item xs={12}>
+//                     <Typography variant="h3" className={classes.title}>
+//                         Welcome to our website!
+//                     </Typography>
+//                     <Typography variant="subtitle1" className={classes.subtitle}>
+//                         We provide the best service and products to our customers.
+//                     </Typography>
+//                     <Button variant="contained" color="primary" className={classes.button}>
+//                         Learn more
+//                     </Button>
+//                     <Button variant="outlined" color="primary" className={classes.button}>
+//                         Contact us
+//                     </Button>
+//                 </Grid>
+//                 <Grid item xs={12} sm={6}>
+//                     <img src="/images/product.png" alt="Product" />
+//                 </Grid>
+//                 <Grid item xs={12} sm={6}>
+//                     <Typography variant="h4" className={classes.title}>
+//                         Our products
+//                     </Typography>
+//                     <Typography variant="body1" className={classes.subtitle}>
+//                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+//                     </Typography>
+//                     <Button variant="contained" color="primary" className={classes.button}>
+//                         View products
+//                     </Button>
+//                 </Grid>
+//             </Grid>
+//         </div>
+//     );
+// }
+
+// export default Homepage;
